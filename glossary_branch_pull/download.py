dbdcf487093b3ab4d794559307e848b4eeea8107
desmondwong1215 <desmondwong1215@gmail.com>
2026-01-19 13:39:27 +0800

    testing

--- glossary_branch_pull/download.py
+++ glossary_branch_pull/download.py
@@ -1,10 +1,6 @@
-import os
-import time
-
 from exercise_utils.cli import run_command
 from exercise_utils.file import create_or_update_file
-from exercise_utils.gitmastery import create_start_tag
-from exercise_utils.git import add, add_remote, checkout, clone_repo_with_git, commit, push
+from exercise_utils.git import add, checkout, clone_repo_with_git, commit, push
 from exercise_utils.github_cli import (
     get_github_username,
     fork_repo,
@@ -14,7 +10,6 @@ from exercise_utils.github_cli import (
 
 TARGET_REPO = "git-mastery/samplerepo-funny-glossary"
 FORK_NAME = "gitmastery-samplerepo-funny-glossary"
-LOCAL_DIR = "funny-glossary"
 
 __resources__ = {}
 
@@ -27,15 +22,7 @@ def setup(verbose: bool = False):
         delete_repo(full_repo_name, verbose)
 
     fork_repo(TARGET_REPO, FORK_NAME, verbose, False)
-    print("Waiting for GitHub to process the fork...")
     clone_repo_with_git(f"https://github.com/{full_repo_name}", verbose, ".")
-
-    # os.chdir(LOCAL_DIR)
-    
-    # add_remote("origin", f"https://github.com/{full_repo_name}", verbose)
-    # run_command(["git", "fetch", "origin"], verbose)
-    # checkout("main", False, verbose)
-    # run_command(["git", "remote", "set-head", "origin", "--auto"], verbose)
 
     run_command(["git", "branch", "-dr", "origin/VWX"], verbose)
 
