bbbe5470e2465b6ff22cce4a010e4795c59442e3
desmondwong1215 <desmondwong1215@gmail.com>
2026-02-24 13:46:39 +0800

    testing

--- test_role_marker/verify.py
+++ test_role_marker/verify.py
@@ -4,11 +4,18 @@ from git_autograder import (
     GitAutograderStatus,
 )
 
+from exercise_utils.github_cli import view_pr
+from exercise_utils.roles import RoleMarker
+
+MISSING_COMMENT = "No comment is found."
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     # INSERT YOUR GRADING CODE HERE
-    print(exercise.read_config("pr_number"))
-    print(exercise.read_config("teammate_role"))
-    print(exercise.read_config("pr_url"))
-
-    return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
+    pr_data = view_pr(1, False)
+    comments = pr_data["comments"]
+    print(comments)
+    print(comments[0]["body"])
+    user_comments = [comment for comment in comments if not RoleMarker.has_role_marker(comment["body"])]
+    if not user_comments:
+        raise exercise.wrong_answer([MISSING_COMMENT])
+    return exercise.to_output(["Good job on adding a comment to the PR!"], GitAutograderStatus.SUCCESSFUL)
