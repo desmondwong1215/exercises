8ac4d19113fdb515e47e9794140c964428eaaa83
desmondwong1215 <desmondwong1215@gmail.com>
2026-03-16 16:11:29 +0800

    testing

--- test_role_marker/verify.py
+++ test_role_marker/verify.py
@@ -9,7 +9,7 @@ MISSING_COMMENT = "No comment is found."
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     # INSERT YOUR GRADING CODE HERE
     print("Starting verification...")
-    if not exercise.repo.prs.pr.comments_from_user:
+    if not exercise.repo.prs.pr.user_comments:
         raise exercise.wrong_answer([MISSING_COMMENT])
     return exercise.to_output(["Good job on adding a comment to the PR!"], GitAutograderStatus.SUCCESSFUL)
 
