f4694a82df1f769dfd02ffdc1644911687adebdc
desmondwong1215 <desmondwong1215@gmail.com>
2026-03-18 10:59:57 +0800

    testing

--- create_pr_from_main/verify.py
+++ create_pr_from_main/verify.py
@@ -6,16 +6,22 @@ from git_autograder import (
 
 from exercise_utils.exercise_config import add_pr_config
 from exercise_utils.github_cli import get_github_username, get_latest_pr_number_by_author
-from .download import TARGET_REPO
+
 
 PR_MISSING = "No PR is found."
+WRONG_BASE_BRANCH = "The PR's base branch is not 'main'."
+
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     username = get_github_username(False)
-    pr_number = get_latest_pr_number_by_author(username, TARGET_REPO, False)
+    target_repo = f"git-mastery/{exercise.config.exercise_repo.repo_title}"
+    pr_number = get_latest_pr_number_by_author(username, target_repo, False)
     if not pr_number:
         raise exercise.wrong_answer([PR_MISSING])
-    add_pr_config(pr_number, TARGET_REPO)
+    add_pr_config(pr_number, target_repo)
     exercise.fetch_pr()
+
+    if exercise.repo.prs.pr.head_branch != "main":
+        raise exercise.wrong_answer([WRONG_BASE_BRANCH])
 
     return exercise.to_output([], GitAutograderStatus.SUCCESSFUL)
