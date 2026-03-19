24651da37ae4531bf237094bdc952a842b9ea816
desmondwong1215 <desmondwong1215@gmail.com>
2026-03-19 08:57:54 +0800

    testing

--- create_pr_from_main/verify.py
+++ create_pr_from_main/verify.py
@@ -7,36 +7,46 @@ from git_autograder import (
 )
 
 from exercise_utils.exercise_config import add_pr_config
-from exercise_utils.github_cli import get_github_username, get_latest_pr_number_by_author
+from exercise_utils.github_cli import get_github_username, get_pr_numbers_by_author
 
 
 PR_MISSING = "No PR is found."
 WRONG_HEAD_BRANCH = "The PR's head branch is not 'main'."
 JAVA_FILE_MISSING = "Java.txt file is missing in the latest commit on main branch."
 JAVA_INVALID_CONTENT = "The content in Java.txt in main branch is not correct."
+MUTIPLE_PRS = "Multiple PRs found. The lastest pr will be used in grading."
 EXPECTED_CONTENT_STEP_3 = ["1955, by James Gosling"]
 
 
 def verify(exercise: GitAutograderExercise) -> GitAutograderOutput:
     username = get_github_username(False)
     target_repo = f"git-mastery/{exercise.config.exercise_repo.repo_title}"
+    comments = []
     
-    pr_number = get_latest_pr_number_by_author(username, target_repo, False)
-    if not pr_number:
+    pr_numbers = get_pr_numbers_by_author(username, target_repo, False)
+    if not pr_numbers:
         raise exercise.wrong_answer([PR_MISSING])
+    if len(pr_numbers) > 1:
+        comments.append(MUTIPLE_PRS)
+    pr_number = pr_numbers[-1]
 
     add_pr_config(pr_number, target_repo, Path("./"))
     exercise.fetch_pr()
 
     if exercise.repo.prs.pr.head_branch != "main":
-        raise exercise.wrong_answer([WRONG_HEAD_BRANCH])
+        comments.append(WRONG_HEAD_BRANCH)
+        raise exercise.wrong_answer(comments)
 
     latest_user_commit = exercise.repo.prs.pr.last_user_commit
+    print("Number of commits in the PR:", len(exercise.repo.prs.pr.commits))
     with latest_user_commit.file("Java.txt") as content:
         if content is None:
-            raise exercise.wrong_answer([JAVA_FILE_MISSING])
+            comments.append(JAVA_FILE_MISSING)
+            raise exercise.wrong_answer(comments)
         extracted_content = [line.strip() for line in content.splitlines() if line.strip() != ""]
     if extracted_content != EXPECTED_CONTENT_STEP_3:
-        raise exercise.wrong_answer([JAVA_INVALID_CONTENT])
+        comments.append(JAVA_INVALID_CONTENT)
+        raise exercise.wrong_answer(comments)
 
-    return exercise.to_output(["Good job creating the PR and pushing commits!"], GitAutograderStatus.SUCCESSFUL)
+    comments.append("Good job creating the PR and pushing commits!")
+    return exercise.to_output(comments, GitAutograderStatus.SUCCESSFUL)
